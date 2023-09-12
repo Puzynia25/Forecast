947cf9f07c468c2c6284c8087158f28e6377bf67
Vitalina Puzynia <102970899+Puzynia25@users.noreply.github.com>
2023-09-13 00:30:48 +0200

    feat: Working with the Highlights component, time conversion from API

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,17 @@
+const PRESSURE_UNITS = 0.750062;
+
 export const capitalizeFirstLetter = (str) => {
   if (!str)  return "";
 
   return str.charAt(0).toUpperCase() + str.slice(1)
 };
+
+export const getPressureMm = (hPa) => {
+  return Math.round(hPa * PRESSURE_UNITS)
+}
+
+export const getTime = (seconds) => {
+  return new Date(seconds * 1000).toLocaleTimeString('en-GB', { timeZone: 'Atlantic/Reykjavik' })
+}
+
+
