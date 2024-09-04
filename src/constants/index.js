e8c9d96beff700c933d2d50f20e9b53c50f1b69d
Vitalina Puzynia <102970899+Puzynia25@users.noreply.github.com>
2024-09-04 10:47:21 +0200

    fix: api_key changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
- export const API_KEY = 'e6703dc8f463c9d4b59f966528e2e65c';
- export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = "a23a2764e2092becd430dd96dcfa2cb6";
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
